754544ace9782667b7bf1e15cb4d160596bd7224
Bhavya Ashok <60926204+bhavya478@users.noreply.github.com>
2024-07-22 11:28:50 -0500

    Add files via upload

---
+++ online_shopping_system/Connection.php
@@ -0,0 +1,14 @@
+<?php
+
+$hostname = "localhost";
+
+$user = "root";
+
+$password = "";
+
+$database = "fashionmode.com";
+
+$db = mysqli_connect($hostname,$user,$password,$database);
+
+
+?>
---
+++ online_shopping_system/Show.php
@@ -0,0 +1,10 @@
+<! DOCTYPE HTML>
+<head>
+<title>M & B Store</title>
+</head>
+<body>
+
+<p align = center><embed src = "NewSlideShow.swf" height = 400 width = 800> </embed></p>
+
+</body>
+<html>
